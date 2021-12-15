45d70fa93d6811eb31e4655a1d53ca8679cf2703
Emilio Wuerges <wuerges@gmail.com>
2021-12-15 15:28:01 -0300

    desiging base data object

--- app/javascript/components/SentenceTranslator.jsx
+++ app/javascript/components/SentenceTranslator.jsx
@@ -1,30 +1,42 @@
 import React, { useState } from "react";
 import { set } from "@rybr/lenses";
 
-function Sentence(props) {
-  const [data, setData] = props.value;
-
-  return (
-    <>
-      <p>Author: {data.author} </p>
-      <input value={data.text} disabled />
-      <input
-        value={data.text}
-        onChange={(e) => setData({ ...data, text: e.target.value })}
-      />
-    </>
-  );
-}
-
 export default function SentenceTranslator() {
-  const [count, setCount] = useState(0);
-  const [data, setData] = useState([{ author: "Author", text: "Some Text" }]);
+  const [data, setData] = useState({
+    config: {
+      target: "pt_BR",
+    },
+    origin: {
+      lang: "en_US",
+      author: "Some Author",
+      text: "Some Text in English",
+    },
+    targets: [
+      {
+        author: "Translator 1",
+        text: "Some Text in Portuguese 1",
+      },
+      {
+        author: "Translator 2",
+        text: "Some Text in Portuguese 2",
+      },
+      {
+        author: "Translator 3",
+        text: "Some Text in Portuguese 3",
+      },
+    ],
+  });
   return (
     <div>
-      <p>YAY from React: {count}</p>
-      <button onClick={() => setCount(count + 1)}>{count}</button>
-      {data.map((d, i) => (
-        <Sentence key={i} value={[d, (t) => setData(set([...data], i, t))]} />
+      <p>
+        {data.origin.lang} {"->"} {data.config.target}
+      </p>
+
+      {data.targets.map((d, i) => (
+        <div key={i}>
+          <input value={d.author} disabled />
+          <input value={d.text} disabled />
+        </div>
       ))}
     </div>
   );
