6fc846445bb3dd55d4375b186ed5049352b3ee4f
Emilio Wuerges <wuerges@gmail.com>
2021-12-16 16:28:12 -0300

    origin/descendant test case added

--- app/models/sentence.rb
+++ app/models/sentence.rb
@@ -1,5 +1,7 @@
 class Sentence < ApplicationRecord
   belongs_to :language
-  belongs_to :origin
+  belongs_to :origin, class_name: "Sentence"
+  # belongs_to :origin, inverse_of: :descendants, class_name: "Sentence", foreign_key: :sentence_id
+  has_many :descendants, inverse_of: :origin, class_name: "Sentence", foreign_key: "origin_id"
   belongs_to :author
 end
--- test/models/sentence_test.rb
+++ test/models/sentence_test.rb
@@ -2,6 +2,21 @@ require "test_helper"
 
 class SentenceTest < ActiveSupport::TestCase
   test "the truth" do
+    sent1 = sentences(:one)
+    sent2 = sentences(:two)
+
+    # Rails.logger.debug "#{sent1.attributes.inspect}"
+    # Rails.logger.debug "#{sent2.attributes.inspect}"
+    # puts "#{sent1.attributes.inspect}"
+    # puts "ORIGIN: #{sent1.origin && sent1.origin.attributes.inspect}"
+    # puts "DESCENDANTS #{sent1.descendants.length}"
+    # puts "#{sent2.attributes.inspect}"
+    # puts "ORIGIN: #{sent2.origin && sent2.origin.attributes.inspect}"
+    # puts "DESCENDANTS #{sent2.descendants.length}"
     
+    assert_equal sent1.origin, sent2
+    assert_equal sent1.descendants.count, 0
+    assert_equal sent2.descendants.count, 1
+    assert_equal sent2.descendants.first, sent1
   end
 end
