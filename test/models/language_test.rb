a21cc5b83c8be795c4434cdb2d6b05d823d47099
Emilio Wuerges <wuerges@gmail.com>
2021-12-16 15:38:36 -0300

    fixtures for languages working

--- test/models/language_test.rb
+++ test/models/language_test.rb
@@ -1,7 +1,10 @@
 require "test_helper"
 
 class LanguageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "languages count" do
+    assert_equal 2, Language.count    
+  end
+  test "check fixture" do
+    assert_equal "pt_BR", languages(:pt_BR).code
+  end
 end
